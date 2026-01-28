d4503d57088a186aef5c16d7c47980dd6a0c7934
Renaldo <estudiante11051987@outlook.com>
2026-01-28 02:06:08 -0300

    Feat: add Animation tree

---
+++ Assets/Game/Script/PlayerMove.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent (typeof (Rigidbody2D), typeof (PlayerInput))]
+public class PlayerMove : MonoBehaviour
+{
+    Rigidbody2D rb2D;
+    Animator animator;
+    Vector2 move;
+    Vector2 faceDir = Vector2.down;
+
+    [SerializeField] float speed = 5f;
+
+    void Awake ()
+    {
+        rb2D = GetComponent<Rigidbody2D> ();
+        animator = GetComponent<Animator> ();
+    }
+
+    void OnMove ( InputValue inputValue )
+    {
+        move = inputValue.Get<Vector2> ();
+        move = Vector2.ClampMagnitude (move, 1f);
+    }
+    void Update ()
+    {
+        if (move.sqrMagnitude > 0.001f)
+            faceDir = move;
+
+        animator.SetFloat ("MoveX", move.x);
+        animator.SetFloat ("MoveY", move.y);
+
+        animator.SetFloat ("FaceX", faceDir.x);
+        animator.SetFloat ("FaceY", faceDir.y);
+
+        animator.SetFloat ("Speed", move.sqrMagnitude);
+
+    }
+    void FixedUpdate ()
+    {
+        rb2D.MovePosition (rb2D.position + move * speed * Time.fixedDeltaTime);
+    }
+}
